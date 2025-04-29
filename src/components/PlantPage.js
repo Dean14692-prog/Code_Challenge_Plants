403cae7e0c62f514b776dd789e7a10cc7a9833a0
Dean14692-prog <dennismuasyangui@gmail.com>
2025-04-29 15:17:53 +0300

    commit

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,39 @@
-import React from "react";
-import NewPlantForm from "./NewPlantForm";
+import React, { useEffect, useState } from "react";
 import PlantList from "./PlantList";
+import NewPlantForm from "./NewPlantForm";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((r) => r.json())
+      .then(setPlants);
+  }, []);
+
+  function handleAddPlant(newPlant) {
+    setPlants([...plants, newPlant]);
+  }
+
+  function handleToggleStock(id) {
+    setPlants((prev) =>
+      prev.map((plant) =>
+        plant.id === id ? { ...plant, soldOut: !plant.soldOut } : plant
+      )
+    );
+  }
+
+  const filteredPlants = plants.filter((plant) =>
+    plant.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onAddPlant={handleAddPlant} />
+      <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      <PlantList plants={filteredPlants} onToggleStock={handleToggleStock} />
     </main>
   );
 }
